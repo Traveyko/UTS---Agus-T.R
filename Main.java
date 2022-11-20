fd227c4273fd9b3d5240c8d0ea305a1b860f07cd
Traveyko <117257434+Traveyko@users.noreply.github.com>
2022-11-20 03:21:01 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public int nilai = 13;
+
+    if (nilai >= 90 && nilai <=100)
+
+        else if (nilai >=80 && nilai <90){
+
+    }
+
+
+}
